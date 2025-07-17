ec38cf16df4216e75b42573284e9e1f0c02127de
Otabek Nabiyev <onabiyev626@gmail.com>
2025-07-17 22:37:15 +0500

    fake port created

--- nimadir/main.go
+++ nimadir/main.go
@@ -2,24 +2,48 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"os"
 	"nimadir/bot"
-	"nimadir/config"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-
 func main() {
-	token := config.GetBotToken()
-	botAPI, err := tgbotapi.NewBotAPI(token)
+	go keepAlive() // Render uchun port ochamiz
+
+	botToken := os.Getenv("BOT_TOKEN")
+	if botToken == "" {
+		log.Fatal("BOT_TOKEN not set")
+	}
+
+	b, err := tgbotapi.NewBotAPI(botToken)
 	if err != nil {
 		log.Panic(err)
 	}
-	botAPI.Debug = false
+	b.Debug = true
+
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
-	updates := botAPI.GetUpdatesChan(u)
+
+	updates := b.GetUpdatesChan(u)
+
 	for update := range updates {
-		bot.HandleUpdate(botAPI, update)
+		bot.HandleUpdate(b, update)
+	}
+}
+
+// Bu funksiya HTTP portni ochib turadi, Render to‘xtatib yubormasligi uchun
+func keepAlive() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "10000"
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Bot is running!"))
+	})
+	log.Printf("Listening on port %s...\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
 	}
 }
